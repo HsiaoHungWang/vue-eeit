0b41d3e2d261c581e74cd7aaa55f2e01eb512d48
hhwang <wang@ispan.com.tw>
2023-09-17 10:31:07 +0800

    修改分頁功能

--- src/components/Paging.vue
+++ src/components/Paging.vue
@@ -1,40 +1,40 @@
 <template>
-           <nav>
-      <ul class="pagination pagination-sm">
-    <li class="page-item" v-for="(value, index) in pages" :key="index" 
-        @click="clickHandler(value)">
-        <a :class="{'currentPage':thePage === value,'page-link':true}">{{value}}</a>
-    </li>  
-  </ul>    </nav>
-
+    <nav>
+        <ul class="pagination pagination-sm">
+            <li class="page-item" v-for="(value, index) in totalPages" :key="index" @click="clickHandler(value)">
+                <a :class="{ 'currentPage': thePage === value, 'page-link': true }">{{ value }}</a>
+            </li>
+        </ul>
+    </nav>
 </template>
     
 <script setup>
 //定義父元件傳進來的屬姓名稱
 //父傳子
-    const props = defineProps({
-        pages:Number,
-        thePage:Number
-    })
+const props = defineProps({
+    totalPages: Number,
+    thePage: Number
+})
 
-    //定義父元件傳進來的事件名稱
-    const emit  = defineEmits(["abcClick"])
+console.log(props.totalPages)
 
-    const clickHandler = page =>{
-        //引發父元件定義好的事件
-        //並且把資料傳給父元件的事件綁定的函式
-        //子傳父
-        emit("abcClick", page)
-    }
+//定義父元件傳進來的事件名稱
+const emit = defineEmits(["childClick"])
+
+const clickHandler = page => {
+    //引發父元件定義好的事件
+    //並且把資料傳給父元件的事件綁定的函式
+    //子傳父
+    emit("childClick", page)
+}
 </script>
     
 <style scoped>
-    
-    .pagination li{
-        cursor: pointer;
-    }
+.pagination li {
+    cursor: pointer;
+}
 
-    .currentPage{
-        background-color: lightblue;
-    }
+.currentPage {
+    background-color: lightblue;
+}
 </style>
